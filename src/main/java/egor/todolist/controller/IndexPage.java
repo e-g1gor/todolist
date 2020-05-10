ede9427ef1d9b160246504ae9eb7a578436d21c7
Egor Klenin <e.g2gor@gmail.com>
2020-05-11 01:15:16 +0300

    dao clean

--- src/main/java/egor/todolist/controller/IndexPage.java
+++ src/main/java/egor/todolist/controller/IndexPage.java
@@ -3,15 +3,10 @@ package egor.todolist.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import egor.todolist.dao.CardService;
 import egor.todolist.dao.ListService;
-import egor.todolist.model.Card;
 import egor.todolist.model.CardList;
 
 @Controller
