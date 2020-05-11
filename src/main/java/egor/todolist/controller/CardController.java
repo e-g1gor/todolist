c8b2668831acf89383ac668c1604d35ce8ac085f
Egor Klenin <e.g2gor@gmail.com>
2020-05-11 11:34:42 +0300

    html forms api

---
+++ src/main/java/egor/todolist/controller/CardController.java
@@ -0,0 +1,42 @@
+package egor.todolist.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import egor.todolist.dao.CardService;
+import egor.todolist.dao.ListService;
+import egor.todolist.model.Card;
+import lombok.extern.java.Log;
+
+@Controller
+@Log
+public class CardController {
+
+  @Autowired
+  CardService cardService;
+  @Autowired
+  ListService listService;
+
+  @PostMapping(path = "/nojs/cards/delete", consumes = "application/x-www-form-urlencoded")
+  public RedirectView deleteCard(Card newCard) {
+      log.info(newCard.toString());
+      cardService.deleteCard(newCard.getId());
+      return new RedirectView("/");
+  }
+
+  @PostMapping(path = "/nojs/cards/add", consumes = "application/x-www-form-urlencoded")
+  public RedirectView addCard(Card newCard) {
+      log.info(newCard.toString());
+      cardService.addCard(newCard);   
+      return new RedirectView("/");
+  }
+
+  @PostMapping(path = "/nojs/cards/update", consumes = "application/x-www-form-urlencoded")
+  public RedirectView udpateCard(Card newCard) {
+      log.info(newCard.toString());
+      cardService.updateCard(newCard);
+      return new RedirectView("/");
+  }
+}
--- src/main/java/egor/todolist/controller/IndexPage.java
+++ src/main/java/egor/todolist/controller/IndexPage.java
@@ -3,19 +3,14 @@ package egor.todolist.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import egor.todolist.dao.CardService;
 import egor.todolist.dao.ListService;
-import egor.todolist.model.Card;
 import egor.todolist.model.CardList;
-import lombok.extern.java.Log;
 
 @Controller
-@Log
 public class IndexPage {
 
     @Autowired
@@ -23,33 +18,22 @@ public class IndexPage {
     @Autowired
     ListService listService;
 
-    @GetMapping({ "/" })
-    public String hello(Model model) {
+    @GetMapping({ "/"})
+    public String hello(Model model, 
+    @RequestParam(value="edited", required=false) Boolean edited,
+    @RequestParam(value="name", required=false) String name,
+    @RequestParam(value="id", required=false) Long id,
+    @RequestParam(value="list", required=false) Long listid,
+    @RequestParam(value="order", required=false) Long order) {
         Iterable<CardList> lists = listService.findAll();
         for (CardList list : lists)
             list.setCards(cardService.findByList(list.getId()));
-        model.addAttribute("lists", lists);
+            model.addAttribute("lists", lists);
+            model.addAttribute("edited", edited);
+            model.addAttribute("name", name);
+            model.addAttribute("id", id);
+            model.addAttribute("list", listid);
+            model.addAttribute("order", order);
         return "index";
-    }
-
-    @DeleteMapping(path = "/nojs/cards", consumes = "application/x-www-form-urlencoded")
-    public String deleteCard(Card newCard) {
-        log.info(newCard.toString());
-        // cardService.deleteCard(newCard.getId());
-        return "redirect:/";
-    }
-
-    @PostMapping(path = "/nojs/cards", consumes = "application/x-www-form-urlencoded")
-    public String addCard(Card newCard) {
-        cardService.addCard(newCard);
-        log.info(newCard.toString());
-        return "redirect:/";
-    }
-
-    @PatchMapping(path = "/nojs/cards", consumes = "application/x-www-form-urlencoded")
-    public String udpateCard(Card newCard) {
-        log.info(newCard.toString());
-        cardService.updateCard(newCard);
-        return "redirect:/";
     }
 }
