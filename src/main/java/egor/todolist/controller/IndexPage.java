e56435debe37623b1e26b5c6ee1367d077178ec1
Egor Klenin <e.g2gor@gmail.com>
2020-05-11 09:39:59 +0300

    cards plain controller get post update

--- src/main/java/egor/todolist/controller/IndexPage.java
+++ src/main/java/egor/todolist/controller/IndexPage.java
@@ -3,13 +3,19 @@ package egor.todolist.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import egor.todolist.dao.CardService;
 import egor.todolist.dao.ListService;
+import egor.todolist.model.Card;
 import egor.todolist.model.CardList;
+import lombok.extern.java.Log;
 
 @Controller
+@Log
 public class IndexPage {
 
     @Autowired
@@ -24,5 +30,26 @@ public class IndexPage {
             list.setCards(cardService.findByList(list.getId()));
         model.addAttribute("lists", lists);
         return "index";
+    }
+
+    @DeleteMapping(path = "/nojs/cards", consumes = "application/x-www-form-urlencoded")
+    public String deleteCard(Card newCard) {
+        log.info(newCard.toString());
+        // cardService.deleteCard(newCard.getId());
+        return "redirect:/";
+    }
+
+    @PostMapping(path = "/nojs/cards", consumes = "application/x-www-form-urlencoded")
+    public String addCard(Card newCard) {
+        cardService.addCard(newCard);
+        log.info(newCard.toString());
+        return "redirect:/";
+    }
+
+    @PatchMapping(path = "/nojs/cards", consumes = "application/x-www-form-urlencoded")
+    public String udpateCard(Card newCard) {
+        log.info(newCard.toString());
+        cardService.updateCard(newCard);
+        return "redirect:/";
     }
 }
